e86ac622d5d5992a9d720935cfc1c9db013d5b2f
strubium <113206902+strubium@users.noreply.github.com>
2024-02-21 09:35:54 -0600

    Create JukeboxProvider.java

---
+++ src/main/java/mcjty/theoneprobe/apiimpl/providers/JukeboxProvider.java
@@ -0,0 +1,43 @@
+package mcjty.theoneprobe.apiimpl.providers;
+
+import mcjty.theoneprobe.api.*;
+import mcjty.theoneprobe.Utilities;
+import net.minecraft.block.BlockJukebox;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemRecord;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+
+public class JukeboxProvider implements IProbeInfoProvider {
+
+    @Override
+    public String getID() {
+        return Utilities.getProviderId("jukebox");
+    }
+
+    @Override
+    public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, @Nonnull IBlockState blockState, IProbeHitData data) {
+        if (blockState.getBlock() instanceof BlockJukebox) {
+            TileEntity tileEntity = world.getTileEntity(data.getPos());
+            if (tileEntity instanceof BlockJukebox.TileEntityJukebox) {
+                BlockJukebox.TileEntityJukebox jukebox = (BlockJukebox.TileEntityJukebox) tileEntity;
+
+                ItemStack record = jukebox.getRecord();
+                if (record.isEmpty()) {
+                    probeInfo.text(TextStyleClass.WARNING + "{*topextras.top.empty*}");
+                    return;
+                }
+
+                IProbeInfo horizontalPane = probeInfo.horizontal(probeInfo.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER));
+                horizontalPane.item(record);
+                String recordName = (record.getItem() instanceof ItemRecord) ? ((ItemRecord) record.getItem()).getRecordNameLocal() : record.getDisplayName();
+                horizontalPane.text(TextStyleClass.INFO + "{*topextras.top.jukebox_record*} " + I18n.format(recordName));
+            }
+        }
+    }
+}
