faf5c2aa3c943e990ded6f4b29f15872b7875fba
strubium <113206902+strubium@users.noreply.github.com>
2024-05-28 16:57:35 -0500

    More lang

--- src/main/java/mcjty/theoneprobe/config/TopModConfigGui.java
+++ src/main/java/mcjty/theoneprobe/config/TopModConfigGui.java
@@ -2,13 +2,17 @@ package mcjty.theoneprobe.config;
 
 import mcjty.theoneprobe.TheOneProbe;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
 import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.fml.client.config.GuiConfig;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
+@SideOnly(Side.CLIENT)
 public class TopModConfigGui extends GuiConfig {
 
     public TopModConfigGui(GuiScreen parentScreen) {
         super(parentScreen, new ConfigElement(ConfigSetup.mainConfig.getCategory(ConfigSetup.CATEGORY_CLIENT)).getChildElements(),
-                TheOneProbe.MODID, false, false, "The One Probe Config");
+                TheOneProbe.MODID, false, false, I18n.format("config.theoneprobe.config.title"));
     }
 }
