4805e2c8aabe675d156ac7872eec8e3ef02acbd9
strubium <113206902+strubium@users.noreply.github.com>
2024-06-01 14:07:05 -0500

    Fix version

--- src/main/java/mcjty/theoneprobe/TheOneProbe.java
+++ src/main/java/mcjty/theoneprobe/TheOneProbe.java
@@ -26,7 +26,7 @@ import java.util.function.Function;
         guiFactory = "mcjty.theoneprobe.config.TopModGuiFactory")
 public class TheOneProbe {
     public static final String MODID = "theoneprobe";
-    public static final String VERSION = "Dev-3";
+    public static final String VERSION = "1.4.28";
     public static final String MIN_FORGE11_VER = "13.19.0.2176";
 
     @SidedProxy(clientSide="mcjty.theoneprobe.setup.ClientProxy", serverSide="mcjty.theoneprobe.setup.ServerProxy")
