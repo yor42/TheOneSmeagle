a3870e8e84c6f119390ff232873d36c139c2abce
strubium <113206902+strubium@users.noreply.github.com>
2024-10-08 17:29:01 -0500

    Improve the Config Screen, bump to dev-11

--- src/main/java/mcjty/theoneprobe/config/TopModConfigGui.java
+++ src/main/java/mcjty/theoneprobe/config/TopModConfigGui.java
@@ -7,6 +7,10 @@ import net.minecraftforge.common.config.ConfigElement;
 import net.minecraftforge.fml.client.config.GuiConfig;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.client.config.IConfigElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Config GUI for The One Probe
@@ -17,7 +21,22 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class TopModConfigGui extends GuiConfig {
 
     public TopModConfigGui(GuiScreen parentScreen) {
-        super(parentScreen, new ConfigElement(ConfigSetup.mainConfig.getCategory(ConfigSetup.CATEGORY_CLIENT)).getChildElements(),
-                TheOneProbe.MODID, false, false, I18n.format("config.theoneprobe.config.title"));
+        super(parentScreen, getConfigElements(), TheOneProbe.MODID, false, false, I18n.format("config.theoneprobe.config.title"));
+    }
+
+    /**
+     * This method creates a list of config elements, each corresponding to a category.
+     * Each element will be shown as a separate button in the GUI.
+     */
+    private static List<IConfigElement> getConfigElements() {
+        List<IConfigElement> configElements = new ArrayList<>();
+
+        // Add a button for CATEGORY_CLIENT
+        configElements.add(new ConfigElement(ConfigSetup.mainConfig.getCategory(ConfigSetup.CATEGORY_CLIENT)));
+
+        // Add a button for CATEGORY_THEONEPROBE
+        configElements.add(new ConfigElement(ConfigSetup.mainConfig.getCategory(ConfigSetup.CATEGORY_THEONEPROBE)));
+
+        return configElements;
     }
 }
