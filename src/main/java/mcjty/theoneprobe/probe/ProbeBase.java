2226c9f7a8725b52cacc021216d60d67ecd02710
strubium <113206902+strubium@users.noreply.github.com>
2024-10-15 15:21:57 -0500

    Cleanup

--- src/main/java/mcjty/theoneprobe/probe/ProbeBase.java
+++ src/main/java/mcjty/theoneprobe/probe/ProbeBase.java
@@ -1,13 +1,11 @@
 package mcjty.theoneprobe.probe;
 
 import mcjty.theoneprobe.TheOneProbe;
-import mcjty.theoneprobe.items.ModItems;
 import mcjty.theoneprobe.setup.GuiProxy;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
